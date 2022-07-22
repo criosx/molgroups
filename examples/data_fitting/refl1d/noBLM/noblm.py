0b21a78d9fa6096a14f2b16ad9683481f5b51b3e
criosx <mail@frank-heinrich.net>
2022-07-22 23:56:44 +0200

    Continue noblm fit example under refl1d. This allows for molecular components directly on a substrate without bilayer.

--- examples/data_fitting/refl1d/noBLM/noblm.py
+++ examples/data_fitting/refl1d/noBLM/noblm.py
@@ -11,7 +11,7 @@ from refl1d.flayer import FunctionalProfile
 # traditionally called bilayer, although there is no bilayer in this example
 
 
-def bilayer(z, sigma, bulknsld, global_rough, rho_substrate, l_surfasil, n_surfasil):
+def bilayer(z, sigma, bulknsld, global_rough, rho_substrate, l_surfasil, vf_surfasil):
     """ Fairly generic bilayer. This assumes a stack of materials already existing because siox.l is set to zero """
 
     # Set unused parameters
@@ -19,9 +19,10 @@ def bilayer(z, sigma, bulknsld, global_rough, rho_substrate, l_surfasil, n_surfa
     # Scale all SLDs from Refl1D units (1e-6 Ang^-2) to molgroups units (Ang^-2)
     bulknsld *= 1e-6
     rho_substrate *= 1e-6
-
+    volume = l_surfasil * 100 * vf_surfasil
     substrate.fnSet(nSL=rho_substrate*substrate.vol, sigma=global_rough, position=0)
-    surfasil.fnSet(length=l_surfasil, position=20+0.5*l_surfasil, nf=n_surfasil)
+    surfasil.fnSet(length=l_surfasil, position=20+0.5*l_surfasil, nf=1, volume=volume,
+                   nSL=0.24e-6 * volume)
     surfasil.fnSetSigma(sigma1=global_rough, sigma2=sigma)
 
     # Calculate scattering properties of volume occupied by bilayer
@@ -37,13 +38,13 @@ def bilayer(z, sigma, bulknsld, global_rough, rho_substrate, l_surfasil, n_surfa
 
 # Define bilayer parameters
 l_surfasil = Parameter(name='surfasil thickness', value=60).range(20, 70)
-n_surfasil = Parameter(name='surfasil numfrac', value=5.).range(0.1, 10)
-sigma = Parameter(name='surfasil roughness', value=5).range(2, 3)
-global_rough = Parameter(name ='global roughness', value=5).range(2, 3)
+vf_surfasil = Parameter(name='surfasil volfrac', value=0.9).range(0.8, 1.)
+sigma = Parameter(name='surfasil roughness', value=5).range(2, 10)
+# global_rough = Parameter(name ='global roughness', value=5).range(2, 3)
 
 # Define bilayer object
-substrate = mol.Box2Err(dz=10, dsigma1=0, dsigma2=2, dlength=40, dvolume=2000, dnSL=0, dnumberfraction=1, name='siox')
-surfasil = mol.Box2Err(dz=50, dsigma1=2, dsigma2=2, dlength=60, dvolume=100, dnSL=5e-6*100, dnumberfraction=1, name='surfasil')
+substrate = mol.Box2Err(dz=10, dsigma1=0, dsigma2=2, dlength=40, dvolume=4000, dnSL=0, dnumberfraction=1, name='siox')
+surfasil = mol.Box2Err(dz=50, dsigma1=2, dsigma2=2, dlength=60, dvolume=100, dnSL=0, dnumberfraction=1, name='surfasil')
 
 # Define molgroups space.
 dimension = 300       # Number of steps
@@ -70,11 +71,11 @@ layer_silicon = Slab(material=silicon, thickness=0.0000, interface=0.0000)
 # Use the bilayer definition function to generate the bilayer SLD profile, passing in the relevant parameters.
 # Note that substrate and bulk SLDs are linked to their respective materials.
 mollayer = FunctionalProfile(dimension*stepsize, 0, profile=bilayer, sigma=sigma, bulknsld=d2o.rho,
-                             global_rough=global_rough, rho_substrate=siox.rho, l_surfasil=l_surfasil,
-                             n_surfasil=n_surfasil)
+                             global_rough=layer_siox.interface, rho_substrate=siox.rho, l_surfasil=l_surfasil,
+                             vf_surfasil=vf_surfasil)
 mollayerh = FunctionalProfile(dimension*stepsize, 0, profile=bilayer, sigma=sigma, bulknsld=h2o.rho,
-                              global_rough=global_rough, rho_substrate=siox.rho, l_surfasil=l_surfasil,
-                             n_surfasil=n_surfasil)
+                              global_rough=layer_siox.interface, rho_substrate=siox.rho, l_surfasil=l_surfasil,
+                             vf_surfasil=vf_surfasil)
 
 # Stack the layers into individual samples, using common layer objects for layers that are unchanged between samples
 # As a convention, always build the sample from the substrate up. If the neutron beam is incident from the substrate
@@ -86,9 +87,9 @@ sampleh = layer_silicon | layer_siox | mollayerh | layer_h2o
 # Set sample parameter ranges and constraints between layer properties
 
 # nSLD parameters
-d2o.rho.range(5.3000, 6.5000)
-h2o.rho.range(-0.6, 0.6)
-siox.rho.range(3.1000, 5.1000)
+d2o.rho.range(5.6, 6.4)
+h2o.rho.range(4.0, 5.6)
+siox.rho.range(3.1000, 3.7000)
 
 # layer thickness parameters
 layer_siox.thickness.range(20, 80)
@@ -104,8 +105,10 @@ layer_siox.interface.range(2.0000, 3.000)
 layer_silicon.interface = layer_siox.interface
 
 # === Data files ===
-probe = load4('../ssBLM/ch061.refl', back_reflectivity=True)
-probeh = load4('../ssBLM/ch060.refl', back_reflectivity=True)
+probe = load4('data0.txt', back_reflectivity=True)
+probeh = load4('data1.txt', back_reflectivity=True)
+#probe = load4('ch060.refl', back_reflectivity=True)
+#probeh = load4('ch061.refl', back_reflectivity=True)
 
 # Set instrumental (probe) parameters
 probe.background.range(-1e-7, 1e-5)
@@ -114,8 +117,8 @@ probe.intensity.range(0.9, 1.05)
 probeh.intensity = probe.intensity
 probe.theta_offset.range(-0.015, 0.005)
 probeh.theta_offset = probe.theta_offset
-probe.sample_broadening.range(-0.005, 0.02)
-probeh.sample_broadening = probe.sample_broadening
+# probe.sample_broadening.range(-0.005, 0.02)
+# probeh.sample_broadening = probe.sample_broadening
 
 # Define critical edge oversampling for samples that require it
 probe.critical_edge(substrate=silicon, surface=d2o)
@@ -128,7 +131,7 @@ probe.critical_edge(substrate=silicon, surface=d2o)
 # approximation is used to account for roughness.  This approximation speeds up
 # the calculation tremendously, and is reasonably accuarate as long as the
 # roughness is much less than the layer thickness
-step = False
+step = True
 
 model = Experiment(sample=sample, probe=probe, dz=stepsize, step_interfaces = step)
 modelh = Experiment(sample=sampleh, probe=probeh, dz=stepsize, step_interfaces = step)
