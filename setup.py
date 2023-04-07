af447434088c99a18b5d40f2508c5a51e87fdcbc
criosx <mail@frank-heinrich.net>
2023-04-06 22:37:07 -0400

    SANS prediction widget finished.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name='molgroups',
-    version='0.1.0',
-    packages=['molgroups', 'molgroups.infotheory', 'molgroups.support'],
+    version='0.2.0',
+    packages=['molgroups'],
     url='https://github.com/criosx/molgroups',
     license='GNU GENERAL PUBLIC LICENSE, Version 3',
     author='Frank Heinrich, David Hoogerheide, Alyssa Thomas',
     author_email='mail@frank-heinrich.net',
     description='Molecular Modeling for Scattering Data Analysis',
     install_requires=[
-        "bumps", "refl1d", "periodictable", "scipy", "numpy", "matplotlib", "pandas", "scikit-learn", "shapely",
-        "sasmodels", "sasdata", "dill", "gpcam"
+        "bumps", "refl1d", "periodictable", "scipy", "numpy", "matplotlib", "pandas", "scikit-learn",
+        "sasmodels", "sasdata", "dill",
     ]
 )
