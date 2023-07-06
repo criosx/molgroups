4f71f88946d8c21b4aab63b175dc3eac4cd6f0ab
criosx <mail@frank-heinrich.net>
2023-07-06 15:08:15 -0400

    Added lipid monolayer for the air/water interface. Needs testing.

--- molgroups/mol.py
+++ molgroups/mol.py
@@ -1072,7 +1072,6 @@ class BLM(CompositenSLDObj):
         return rdict
 
 
-'''
 class Monolayer(CompositenSLDObj, BLM):
     def __init__(self, lipids=None, lipid_nf=None, xray_wavelength=None, name='monolayer', **kwargs):
         """
@@ -1207,7 +1206,6 @@ class Monolayer(CompositenSLDObj, BLM):
             rdict[cName]['water in outer headgroups'] = 1 - rdict[cName]['water in outer headgroups']
 
         return rdict
-'''
 
 class ssBLM(BLM):
     def __init__(self, inner_lipids=None, inner_lipid_nf=None, outer_lipids=None, outer_lipid_nf=None, lipids=None,
