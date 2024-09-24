6100d86550f6b5b9488d609867659245a2b3c186
hoogerheide <david.hoogerheide@nist.gov>
2024-09-24 11:01:48 -0400

    reorganize refl1d_interface

---
+++ molgroups/refl1d_interface/__init__.py
@@ -0,0 +1,3 @@
+from molgroups.refl1d_interface.experiment import MolgroupsExperiment, make_samples
+from molgroups.refl1d_interface.layers import MolgroupsStack, MolgroupsLayer
+from molgroups.refl1d_interface.groups import SubstrateInterface, ssBLMInterface, tBLMInterface
