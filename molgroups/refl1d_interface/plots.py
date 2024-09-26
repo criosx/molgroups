92641572c5da1a6e0a65ecb07382bb843a88063a
hoogerheide <david.hoogerheide@nist.gov>
2024-09-26 16:32:28 -0400

    CVO uncertainty plot, attached to MolgroupsLayer

--- molgroups/refl1d_interface/plots.py
+++ molgroups/refl1d_interface/plots.py
@@ -1,11 +1,13 @@
 """Plots for the Refl1D webview interface
     Used with MolgroupsExperiment.register_webview_plot
 """
+import time
 import numpy as np
 import plotly.graph_objs as go
 
 from bumps.dream.state import MCMCDraw
 from bumps.webview.server.custom_plot import CustomWebviewPlot
+from bumps.plotutil import form_quantiles
 from refl1d.names import FitProblem, Experiment
 from refl1d.webview.server.colors import COLORS
 
@@ -101,7 +103,7 @@ def cvo_plot(layer: MolgroupsLayer, model: Experiment | None = None, problem: Fi
     fig.update_layout(
         title='Component Volume Occupancy',
         template = 'plotly_white',
-        xaxis_title=dict(text='z (Ang)'),
+        xaxis_title=dict(text='z (\u212b)'),
         yaxis_title=dict(text='volume occupancy'),
         legend=dict(yanchor='top',
                     xanchor='right',
@@ -112,3 +114,159 @@ def cvo_plot(layer: MolgroupsLayer, model: Experiment | None = None, problem: Fi
 
     return CustomWebviewPlot(fig_type='plotly',
                                 plotdata=fig)
+
+# =============== Uncertainty plot ================
+
+def cvo_uncertainty_plot(layer: MolgroupsLayer, model: Experiment | None = None, problem: FitProblem | None = None, state: MCMCDraw | None = None):
+
+    # TODO: allow groups to label some items as uncertainty groups and use the median or best for others
+
+    if state is None:
+        return cvo_plot(layer, model, problem, state)
+    
+    fig = go.Figure()
+    traces = []
+    uncertainty_traces = []
+    MOD_COLORS = COLORS[1:]
+    color_idx = 1
+    sumarea = 0
+
+    group_names = {}
+    for group in [layer.base_group] + layer.add_groups + layer.overlay_groups:
+        for k, v in group._group_names.items():
+            # merge group names
+            if k not in group_names.keys():
+                group_names[k] = []
+            group_names[k] += v
+
+    statdata: dict[str, list[np.ndarray]] = {lbl: [] for lbl in group_names.keys()}
+    statnormarea = []
+    print('Starting CVO uncertainty calculation...')
+    init_time = time.time()
+
+    for pt in state.draw().points[:500]:
+        problem.setp(pt)
+        model.update()
+        model.nllf()
+        imoldat = {}
+        for group in [layer.base_group] + layer.add_groups + layer.overlay_groups:
+            for k, v in group._group_names.items():
+                for gp in v:
+                    group._stored_profile[gp]['frac_replacement'] = group._stored_profile['frac_replacement']
+
+            imoldat.update(group._stored_profile)
+
+        for lbl, item in group_names.items():
+            area = 0
+            for gp in item:
+                if gp in imoldat.keys():
+                    zaxis = imoldat[gp]['zaxis']
+                    newarea = imoldat[gp]['area'] / imoldat[gp]['frac_replacement']
+                    area += np.maximum(0, newarea)
+            statdata[lbl].append(area / imoldat['normarea'])
+        statnormarea.append(imoldat['normarea'])
+    print(f'CVO uncertainty calculation done after {time.time() - init_time} seconds')
+
+    for lbl, statlist in statdata.items():
+        color = MOD_COLORS[color_idx % len(MOD_COLORS)]
+        plotly_color = ','.join(map(str, hex_to_rgb(color)))
+        med_area = np.median(statlist, axis=0)
+        med_norm_area = np.median(statnormarea)
+        #print(lbl, med_area.shape, zaxis.shape)
+        _, q = form_quantiles(statlist, (68,))
+        for lo, hi in q:
+            uncertainty_traces.append(go.Scatter(x=zaxis,
+                                    y=lo, # * med_norm_area / normarea,
+                                    mode='lines',
+                                    line=dict(color=color, width=1),
+                                    hoverinfo="skip",
+                                    fill='tonexty',
+                                    fillcolor=f'rgba({plotly_color},0.3)',
+                                    showlegend=False
+                                    ))
+            uncertainty_traces.append(go.Scatter(x=zaxis,
+                                    y=hi, # * med_norm_area / normarea,
+                                    showlegend=False, 
+                                    opacity=0.3,
+                                    hoverinfo="skip",
+                                    mode='lines',
+                                    name=lbl,
+                                    line=dict(color=color, width=1)))
+            uncertainty_traces.append(go.Scatter(x=zaxis,
+                                y=med_area, # * med_norm_area / normarea,
+                                mode='lines',
+                                name=lbl,
+                                line=dict(color=color)))
+
+        color_idx += 1
+        sumarea += med_area * med_norm_area
+
+    """
+    for lbl, item in group_names.items():
+        area = 0
+        for gp in item:
+            if gp in moldat.keys():
+                zaxis = moldat[gp]['zaxis']
+                area += np.maximum(0, moldat[gp]['area'])
+            else:
+                print(f'Warning: {gp} not found')
+
+        color = MOD_COLORS[color_idx % len(MOD_COLORS)]
+        plotly_color = ','.join(map(str, hex_to_rgb(color)))
+        traces.append(go.Scatter(x=zaxis,
+                                 y=area / normarea,
+                                 mode='lines',
+                                 name=lbl,
+                                 line=dict(color=color)))
+        traces.append(go.Scatter(x=zaxis,
+                                 y=area / normarea,
+                                 mode='lines',
+                                 line=dict(width=0),
+                                 fill='tozeroy',
+                                 fillcolor=f'rgba({plotly_color},0.3)',
+                                 showlegend=False
+                                 ))
+        color_idx += 1
+        sumarea += area
+    """
+    color = COLORS[0]
+    plotly_color = ','.join(map(str, hex_to_rgb(color)))
+    
+    buffer_traces = []
+
+    buffer_traces.append(go.Scatter(x=zaxis,
+                                y=sumarea / med_norm_area,
+                                mode='lines',
+                                name='buffer',
+                                line=dict(color=color)))
+    buffer_traces.append(go.Scatter(x=zaxis,
+                                y=sumarea / med_norm_area,
+                                mode='lines',
+                                line=dict(width=0),
+                                fill='tonexty',
+                                fillcolor=f'rgba({plotly_color},0.3)',
+                                showlegend=False
+                                ))    
+    buffer_traces.append(go.Scatter(x=zaxis,
+                                y=[1.0] * len(zaxis),
+                                mode='lines',
+                                line=dict(color=color, width=0),
+                                showlegend=False))
+
+    
+    fig.add_traces((traces + uncertainty_traces + buffer_traces)[::-1])
+
+    fig.update_layout(
+        title='Component Volume Occupancy with Uncertainty',
+        template = 'plotly_white',
+        xaxis_title=dict(text='z (\u212b)'),
+        yaxis_title=dict(text='volume occupancy'),
+        legend=dict(yanchor='top',
+                    y=0.98,
+                    xanchor='right',
+                    x=0.99),
+        yaxis_range=[0, 1]
+    )
+
+    return CustomWebviewPlot(fig_type='plotly',
+                             plotdata=fig)
